5e5ad32eddc088288985b59af77eab229ccffd69
Daniel Fernandez <dfp.fernandez@gmail.com>
2020-06-09 08:45:23 -0300

    Show Prepare Cookie button first on mobile web version

--- spec/features/cooking/cookies_spec.rb
+++ spec/features/cooking/cookies_spec.rb
@@ -8,7 +8,7 @@ feature 'Cooking cookies' do
     expect(page).to_not have_content 'Chocolate Chip'
     expect(page).to_not have_content 'Your Cookie is Ready'
 
-    click_link_or_button 'Prepare Cookie'
+    find('.oven-desktop').click_link_or_button('Prepare Cookie')
     fill_in 'Fillings', with: 'Chocolate Chip'
     click_button 'Mix and bake'
 
@@ -24,11 +24,11 @@ feature 'Cooking cookies' do
     oven = FactoryGirl.create(:oven, user: user)
     visit oven_path(oven)
 
-    click_link_or_button 'Prepare Cookie'
+    find('.oven-desktop').click_link_or_button('Prepare Cookie')
     fill_in 'Fillings', with: 'Chocolate Chip'
     click_button 'Mix and bake'
 
-    click_link_or_button  'Prepare Cookie'
+    find('.oven-desktop').click_link_or_button('Prepare Cookie')
     expect(page).to have_content 'A set of cookies are already in the oven!'
     expect(current_path).to eq(oven_path(oven))
     expect(page).to_not have_button 'Mix and bake'
